4ff8de163ee808e2d7a24ff138fc584dd9bb9f68
M Shahid <m_shahid@live.co.uk>
2025-02-28 09:43:19 +0500

    added threads

--- src/main.c
+++ src/main.c
@@ -8,13 +8,61 @@
 #include "pwm_f.h"
 #include "adc_f.h"
 
+#define         STACKSIZE               2048
+#define         ADC_THREAD_PRIORITY     6
+#define         ADC_SLEEP_TIME          500
+
 #define         SOL1_NODE               DT_ALIAS(sol1)
 #define         SOL1EXT_NODE            DT_ALIAS(sol1ext)
 #define         SOL2_NODE               DT_ALIAS(sol2)
 #define         SOL2EXT_NODE            DT_ALIAS(sol2ext)
 #define         BLDC_NODE               DT_ALIAS(bldc)
 
+#define         VBAT_THRESHOLD          10000
+#define         CR1_THRESHOLD           300
+#define         CR2_THRESHOLD           300
+
 uint16_t adc_data[3] = {0};
+
+static void timer0_handler(struct k_timer *timer_id)
+{
+        printk("Timer 0 fired!\r\n");
+        // Acquire ADC data
+        adc_data[0] = read_adc(6);      // Battery
+        adc_data[1] = read_adc(4);      // CR1
+        adc_data[2] = read_adc(5);      // CR2
+        printk("ADC 0: %d, ADC 4: %d, ADC 5: %d\r\n", adc_data[0], adc_data[1], adc_data[2]);
+
+        if (adc_data[0] > VBAT_THRESHOLD)
+        {
+                printk("Battery Power detected!\r\n");
+        }
+        else
+        {
+                printk("Battery Power not detected!\r\n");
+        }
+
+        if (adc_data[1] > CR1_THRESHOLD)
+        {
+                printk("CR1 Power detected!\r\n");
+        }
+        else
+        {
+                printk("CR1 Power not detected!\r\n");
+        }
+
+        if (adc_data[2] > CR2_THRESHOLD)
+        {
+                printk("CR2 Power detected!\r\n");
+        }
+        else
+        {
+                printk("CR2 Power not detected!\r\n");
+        }
+
+}
+
+static K_TIMER_DEFINE(timer0, timer0_handler, NULL);
 
 static const struct gpio_dt_spec sol1 = GPIO_DT_SPEC_GET(SOL1_NODE, gpios);
 static const struct gpio_dt_spec sol1ext = GPIO_DT_SPEC_GET(SOL1EXT_NODE, gpios);
@@ -99,7 +147,7 @@ static void app_cmd_cb(uint8_t cmd)
 
 static uint16_t app_data_cb(void)
 {
-        adc_data[0] = read_adc(0);
+        adc_data[0] = read_adc(6);
         adc_data[1] = read_adc(4);
         adc_data[2] = read_adc(5);
         return 0xff;
@@ -175,20 +223,28 @@ int main(void)
 
         adc_init();
         
-        while (1)
-        {
-                // read_adc(0);
-                // k_msleep(2000);
-                // read_adc(2);
-                // k_msleep(2000);
-                // read_adc(4);
-                // k_msleep(2000);
-                // read_adc(5);
-                // k_msleep(2000);
-                // read_adc(6);
-                // k_msleep(2000);
-                // read_adc(7);
-                // k_msleep(2000);
-        }
+        // Start timer 0 once every 0.5 seconds
+        k_timer_start(&timer0, K_MSEC(500), K_MSEC(500));
+
         return 0;
 }
+
+static void adc_thread_func(void *unused1, void *unused2, void *unused3)
+{
+        ARG_UNUSED(unused1);
+        ARG_UNUSED(unused2);
+        ARG_UNUSED(unused3);
+
+        while (1)
+        {
+                printk("ADC Thread fired!\r\n");
+                // Acquire ADC data
+                adc_data[0] = read_adc(6);      // Battery
+                adc_data[1] = read_adc(4);      // CR1
+                adc_data[2] = read_adc(5);      // CR2
+                printk("ADC 6: %d, ADC 4: %d, ADC 5: %d\r\n", adc_data[0], adc_data[1], adc_data[2]);
+                k_sleep(K_MSEC(ADC_SLEEP_TIME));
+        }
+}
+
+K_THREAD_DEFINE(adc_thread_id, STACKSIZE, adc_thread_func, NULL, NULL, NULL, ADC_THREAD_PRIORITY, 0, 0);
